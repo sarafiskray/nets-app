90667ea70c9b7f9fab5e207372906df8bb7f698d
sarafiskray <sarafiskray@gmail.com>
2021-03-20 19:59:31 -0400

    switch between points, rebounds, assists on chart

--- src/StatsChart.js
+++ src/StatsChart.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import './StatsChart.css'
 
 const StatsChart = (props) => {
 
     const {playerOneStats, playerTwoStats } = props;
 
+    const [active, setActive] = useState("points");
   
 
     //make empty array
@@ -33,21 +35,61 @@ const StatsChart = (props) => {
 
     console.log(combinedStats)
 
+    const choosePoints = (e) => {
+        e.preventDefault()
+        setActive("points")
+    }
+
+    const chooseRebounds = (e) => {
+        e.preventDefault()
+        setActive("rebounds")
+    }
+
+    const chooseAssists = (e) => {
+        e.preventDefault()
+        setActive("assists")
+    }
+
+
+
     
     //use playerOneStats.length > 0 : jsx
 
     return (
-        <LineChart width={800} height = {400} data={combinedStats}>
-            { playerOneStats.length > 0 ? (
-            <Line type="monotone" dataKey="p1Pts" stroke="purple"/>
-            ) : null }
-            { playerTwoStats.length > 0 ? (
-            <Line type="monotone" dataKey="p2Pts" stroke="green" />
-            ) : null }
-            <XAxis tick = {false} />
-            <YAxis type="number" domain={[0, 80]}/>
-            <Tooltip cursor={false} />
-        </LineChart>
+        <Fragment>
+            <div className={ active == "points" ? "chip statSelected" : "chip"} onClick={choosePoints}>
+                Points
+            </div>
+            <div className={ active == "rebounds" ? "chip statSelected" : "chip"} onClick={chooseRebounds}>
+                Rebounds
+            </div>
+            <div className={ active == "assists" ? "chip statSelected" : "chip"} onClick={chooseAssists}>
+                Assists
+            </div>
+            <LineChart width={800} height = {400} data={combinedStats}>
+                { playerOneStats.length > 0 && active == "points" ? (
+                <Line type="monotone" dataKey="p1Pts" stroke="purple"/>
+                ) : null }
+                { playerTwoStats.length > 0 && active == "points" ? (
+                <Line type="monotone" dataKey="p2Pts" stroke="green" />
+                ) : null }
+                { playerOneStats.length > 0 && active == "rebounds" ? (
+                <Line type="monotone" dataKey="p1Reb" stroke="purple"/>
+                ) : null }
+                { playerTwoStats.length > 0 && active == "rebounds" ? (
+                <Line type="monotone" dataKey="p2Reb" stroke="green" />
+                ) : null }
+                { playerOneStats.length > 0 && active == "assists" ? (
+                <Line type="monotone" dataKey="p1Asts" stroke="purple"/>
+                ) : null }
+                { playerTwoStats.length > 0 && active == "assists" ? (
+                <Line type="monotone" dataKey="p2Asts" stroke="green" />
+                ) : null }
+                <XAxis tick = {false} />
+                <YAxis type="number" domain={[0, 80]}/>
+                <Tooltip cursor={false} />
+            </LineChart>
+        </Fragment>
     )
 }
 
