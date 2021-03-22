8bdb7a163d43a2b87df3ce6f0a2cf8b791900192
sarafiskray <sarafiskray@gmail.com>
2021-03-21 23:27:52 -0400

    added instructions to search

--- src/Lookup/Results.js
+++ src/Lookup/Results.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React , { Fragment } from 'react';
 
 const Results = (props) => {
 
     const {listPlayers} = props;
 
-    return(
-        <ul className="collection">
-            { listPlayers }
-        </ul>
+    return (
+        <Fragment>
+            <p className="center-align">Enter full first or last name</p>
+            <ul className="collection">
+                { listPlayers }
+            </ul>
+        </Fragment>
     )
 }
 
